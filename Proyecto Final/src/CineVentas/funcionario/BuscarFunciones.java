e38b08eeeafc3756df56ddd22417653d67ce7002
mercycheryl <mercysperugachi@gmail.com>
2025-07-18 23:38:41 -0500

    BuscarFunciones: interfaz

--- Proyecto Final/src/CineVentas/funcionario/BuscarFunciones.java
+++ Proyecto Final/src/CineVentas/funcionario/BuscarFunciones.java
@@ -4,6 +4,12 @@ import javax.swing.*;
 
 public class BuscarFunciones extends JFrame {
     private JPanel panelBuscarFunciones;
+    private JTable table1;
+    private JButton cerrarSesionButton;
+    private JButton venderButton;
+    private JButton buscarButton;
+    private JTextField textField1;
+
     public BuscarFunciones() {
         setTitle("Buscar Funciones");
         setSize(300, 300);
