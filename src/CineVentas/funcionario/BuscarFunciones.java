53ce0b0e8d35a4752dbb2636409962044da65ded
mercycheryl <mercysperugachi@gmail.com>
2025-07-19 22:40:38 -0500

    Update BuscarFunciones.java

--- src/CineVentas/funcionario/BuscarFunciones.java
+++ src/CineVentas/funcionario/BuscarFunciones.java
@@ -1,4 +1,27 @@
 package CineVentas.funcionario;
 
-public class BuscarFunciones {
+import javax.swing.*;
+
+public class BuscarFunciones extends JFrame {
+    private JPanel panelBuscarFunciones;
+    private JTable tbBuscarFunciones;
+    private JButton btnCerrarSesion;
+    private JButton btnVender;
+    private JButton btnBuscar;
+    private JTextField txtBuscar;
+
+    public BuscarFunciones() {
+        setTitle("Buscar Funciones");
+        setSize(300, 300);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(panelBuscarFunciones);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->{
+            new BuscarFunciones().setVisible(true);
+        });
+    }
 }
