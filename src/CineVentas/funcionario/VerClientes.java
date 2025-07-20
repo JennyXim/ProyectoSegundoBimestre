30f25dc971112a85b1b5772154fa1b5eb1ca3609
mercycheryl <mercysperugachi@gmail.com>
2025-07-19 22:59:03 -0500

    Update VerClientes.java

--- src/CineVentas/funcionario/VerClientes.java
+++ src/CineVentas/funcionario/VerClientes.java
@@ -1,4 +1,25 @@
 package CineVentas.funcionario;
 
-public class VerClientes {
+import javax.swing.*;
+
+public class VerClientes extends JFrame {
+    private JPanel panelVerClientes;
+    private JTextField txtVerClientes;
+    private JButton btnBuscar;
+    private JTable tbVerClientes;
+
+    public VerClientes() {
+        setTitle("Ver clientes");
+        setSize(300, 300);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(panelVerClientes);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->{
+            new VerClientes().setVisible(true);
+        });
+    }
 }
