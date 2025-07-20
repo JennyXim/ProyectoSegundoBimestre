708b9b9d70d19cbdfee52c2940bb151ccc1e655b
mercycheryl <mercysperugachi@gmail.com>
2025-07-19 22:57:44 -0500

    Update VenderBoletos.java

--- src/CineVentas/funcionario/VenderBoletos.java
+++ src/CineVentas/funcionario/VenderBoletos.java
@@ -1,4 +1,31 @@
 package CineVentas.funcionario;
 
-public class VenderBoletos {
+import javax.swing.*;
+
+public class VenderBoletos extends JFrame {
+    private JPanel panelVenderBoletos;
+    private JButton btnCalcularTotal;
+    private JButton btnRegistrarVenta;
+    private JButton btnLimpiar;
+    private JComboBox comboBox1;
+    private JTextField txtNombre;
+    private JTextField txtCedula;
+    private JTextField txtCantidadBoletos;
+    private JLabel txtPrecioUnitario;
+    private JLabel txtTotal;
+
+    public VenderBoletos() {
+        setTitle("Venda boletos");
+        setSize(500, 300);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(panelVenderBoletos);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->{
+            new VenderBoletos().setVisible(true);
+        });
+    }
 }
