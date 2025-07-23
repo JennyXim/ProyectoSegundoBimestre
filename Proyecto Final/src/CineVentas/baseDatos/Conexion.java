dbd1bc648980e141a3098ce3f45b503d9113e80a
sebastianh-hash <snhidalgop03@gmail.com>
2025-07-23 12:09:06 -0500

    Update Conexion.java

--- Proyecto Final/src/CineVentas/baseDatos/Conexion.java
+++ Proyecto Final/src/CineVentas/baseDatos/Conexion.java
@@ -1,4 +1,20 @@
 package CineVentas.baseDatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String URL = "jdbc:sqlite:base_datos/cine.db"; // Ruta de la base de datos
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar con la base de datos:");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
+
